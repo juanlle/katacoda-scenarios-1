65365cdcd11f62d35fe1ac305683139d8b7883d9
defrox <defrostdj@hotmail.com>
2018-12-13 00:36:52 +0100

    Escenario: 12 Ingress

--- 10 Helm/set-env.sh
+++ 10 Helm/set-env.sh
@@ -2,5 +2,5 @@ mkdir /mnt/data1
 mkdir /mnt/data2
 mkdir /mnt/data3
 clear
-echo "\u001b[32mEsta es tu Consola Interactiva.\u001b[m\r\n"
+echo -e "\u001b[32mEsta es tu Consola Interactiva.\u001b[m\r\n"
 launch.sh
