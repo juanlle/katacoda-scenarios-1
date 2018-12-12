1580e6cb27c08cabfa452a0ef123fb6e802fdbf9
defrox <defrostdj@hotmail.com>
2018-12-13 00:39:50 +0100

    Escenario: 12 Ingress

--- 12 Ingress/set-env.sh
+++ 12 Ingress/set-env.sh
@@ -1,5 +1,5 @@
 HOST_IP=$(echo $SSH_CONNECTION | awk '{print $3}')
 sed -i "s/HOST_IP/${HOST_IP}/g" ~/ingress.yaml
 clear
-echo -e "\u001b[32mEsta es tu Consola Interactiva.\u001b[m\r\n"
+echo -ne "\u001b[32mEsta es tu Consola Interactiva.\u001b[m\r\n"
 launch.sh
