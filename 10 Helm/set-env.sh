b841b72a7956cba91427bf0f88419d6a019acc3e
defrox <defrostdj@hotmail.com>
2018-12-13 00:35:31 +0100

    Escenario: 12 Ingress

--- 10 Helm/set-env.sh
+++ 10 Helm/set-env.sh
@@ -1,4 +1,6 @@
 mkdir /mnt/data1
 mkdir /mnt/data2
 mkdir /mnt/data3
+clear
+echo "\u001b[32mEsta es tu Consola Interactiva.\u001b[m\r\n"
 launch.sh
--- 12 Ingress/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-HOST_IP=$(echo $SSH_CONNECTION | awk '{print $3}')
-sed -i "s/HOST_IP/${HOST_IP}/g" ~/ingress.yaml
